a14d0809be91cba8135a3b3dbfc445bc89a7a6f0
Tom <tomnik104@yahoo.de>
2019-03-17 00:24:50 +0100

    fixed a bug where fastify would only listen on localhost

--- src/WebServer.js
+++ src/WebServer.js
@@ -6,14 +6,14 @@
 const fastify = require('fastify')();
 const ydl = require('./youtube-dl');
 
-fastify.register(require('fastify-cors'))
+fastify.register(require('fastify-cors'));
 
 fastify.post('/load', async (request, reply) => {
-    if(request.query.url){
+    if (request.query.url) {
         console.log('Got URL: '.green + request.query.url.blue);
         reply.status(200).send();
-        ydl(request.query.url)
-    }else {
+        ydl(request.query.url);
+    } else {
         reply.status(400).send();
     }
 });
@@ -22,13 +22,13 @@ fastify.get('/v', async (request, reply) => {
     reply.send({
         version: require('../package').version,
         neededV: 1
-    })
+    });
 });
 
 try {
-    fastify.listen(9999);
-    console.log('WebServer started!'.green)
-}catch (e){
+    fastify.listen('0.0.0.0', 9999);
+    console.log('WebServer started!'.green);
+} catch (e) {
     console.log('Unable to start Fastify WebServer'.red);
     throw e;
 }
